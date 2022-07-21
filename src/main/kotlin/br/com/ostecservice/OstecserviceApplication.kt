ed46264c1dc7a4a8f716c621b204635cd5d196e1
Rafael <rafaajuda@hotmail.com>
2022-07-21 20:25:45 -0300

    initial commit

---
+++ src/main/kotlin/br/com/ostecservice/OstecserviceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.ostecservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OstecserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<OstecserviceApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/ostecservice/OstecserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ostecservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OstecserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
